20b105f89bfccfca67a7cf09692665c82674c486
QaorVa <100664948+QaorVa@users.noreply.github.com>
2023-06-04 23:57:32 +0700

    merge update

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function toggleDropdown(element) {
     const dropdownContent = element.nextElementSibling;
     const dropdownContent2 = element.querySelector(".menu-dropdown");
+    dropdownContent2.classList.toggle("show");
     element.classList.toggle("active");
     dropdownContent.classList.toggle("show");
-    dropdownContent2.classList.toggle("show");
+    
   }
